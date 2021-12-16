8f7ae202151714f25f93e62bb440ace8103201d3
terpodia <diegortizte@gmail.com>
2021-12-16 13:06:18 -0400

    New problems solved

---
+++ Problems Solved/Codeforces/Greedy/Forbidden Subsequence.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0); 
+    int TC; cin >> TC;
+
+    while (TC--) {
+        string s, t; cin >> s >> t;
+        int cnt[26];
+        memset(cnt, 0, sizeof(cnt));
+
+        for (char c : s) cnt[c - 'a']++;
+
+        string ans;
+
+        if (t[0] == 'a' && t[1] == 'b' && cnt[0] > 0 && cnt[1] > 0 && cnt[2] > 0) {
+            for (int j=0; j<cnt[0]; j++)
+                ans.push_back('a');
+
+            for (int j=0; j<cnt[2]; j++)
+                ans.push_back('c');
+
+            for (int j=0; j<cnt[1]; j++)
+                ans.push_back('b');
+
+            for (int i=3; i<26; i++) {
+                for (int j=0; j<cnt[i]; j++)
+                    ans.push_back((char)(i + 'a'));
+            }
+        }
+        else {
+            for (int i=0; i<26; i++) {
+                for (int j=0; j<cnt[i]; j++)
+                    ans.push_back((char)(i + 'a'));
+            }
+        }
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
---
+++ Problems Solved/Codeforces/Greedy/Paprika and Permutation.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int aux;
+
+#define sz(x) (int)x.size()
+
+int main() {
+    int TC; aux=scanf("%d",&TC);
+    while (TC--) {
+        int n; aux=scanf("%d",&n);
+        int a[n];
+        for (int i=0; i<n; i++) aux=scanf("%d",a+i);
+
+        bool ok[n+1]; memset(ok, false, sizeof(ok));
+
+        vector<int> cnt;
+
+        for (int i=0; i<n; i++) {
+            if (a[i] >= 1 && a[i] <= n && !ok[a[i]]) ok[a[i]] = true;
+            else cnt.push_back(((a[i] + 1) / 2) - 1);
+        }
+
+        sort(cnt.begin(), cnt.end(), greater<int>());
+
+        int ans = 0; bool can = true;
+        int pos = 0;
+
+        for (int i=n; i>0; i--) {
+            if (!ok[i]) {
+                if (cnt[pos] >= i) pos++, ans++, ok[i] = true;
+                else { can = false; break; }
+            }
+        }
+
+        printf("%d\n", (can) ? ans : -1);
+    }
+
+    return 0;
+}
