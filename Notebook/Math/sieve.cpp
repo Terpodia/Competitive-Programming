89d242d24f6d88db19fda88cfbef6001b5ec7265
terpodia <diegortizte@gmail.com>
2022-01-04 16:42:02 -0400

    Sieve of Eratosthenes algorithm updated

--- Notebook/Math/sieve.cpp
+++ Notebook/Math/sieve.cpp
@@ -17,6 +17,8 @@ void sieve(ll upperbound) {
 
   bs[0] = bs[1] = 0;
 
+  p.push_back(2);
+
   for (ll i=4; i<_sieve_size; i+=2) bs[i] = 0;
   
   for (ll i=3; i<_sieve_size; i+=2) {
@@ -39,16 +41,94 @@ bool isPrime(ll n) {
   return true;
 }
 
+vll primeFactors(ll n) {
+  vll factors;
+  for (int i=0; i < (int)p.size() && p[i] * p[i] <= n; i++) {
+    while (n % p[i] == 0) factors.push_back(p[i]), n /= p[i];
+  }
+
+  if (n > 1) factors.push_back(n);
+  return factors;
+}
+
+int numPF(ll n) {
+  int ret=0;
+
+  for (int i=0; i < (int)p.size() && p[i] * p[i] <= n; i++) {
+    while (n % p[i] == 0) n /= p[i], ret++;
+  }
+
+  if (n > 1) ret++;
+  return ret;
+}
+
+int numDiv(ll n) {
+  int ret=1;
+
+  for (int i=0; i < (int)p.size() && p[i] * p[i] <= n; i++) {
+    int cnt=1;
+    while (n % p[i] == 0) n /= p[i], cnt++;
+
+    ret *= cnt;
+  }
+
+  if (n > 1) ret *= 2;
+  return ret;
+}
+
+ll sumDiv(ll n) {
+  ll ret=1;
+
+  for (int i=0; i < (int)p.size() && p[i] * p[i] <= n; i++) {
+    ll cnt=1, power=p[i];
+    while (n % p[i] == 0) n /= p[i], cnt += power, power *= p[i];
+    
+    ret *= cnt;
+  }
+
+  if (n > 1) ret *= (1 + n);
+
+  return ret;
+}
+
+ll eulerPhi(ll n) {
+  ll ret = n;
+
+  for (int i=0; i < (int)p.size() && p[i] * p[i] <= n; i++) {
+    if (n % p[i] == 0) ret -= ret / p[i];
+    while (n % p[i] == 0) n /= p[i];
+  }
+
+  if (n > 1) ret -= ret / n;
+  return ret;
+}
+
 int main() {
   cin.tie(0)->sync_with_stdio(0); 
 
   sieve(10000000);
 
-  int N; cin >> N;
+  ll N; cin >> N;
 
   cout << "There are " << (int)p.size() << " primes in range [1, " << _sieve_size << ")\n";
 
   (isPrime(N)) ? cout << N << " is prime\n" : cout << N << " is composite\n";
+
+  cout << N << " has " << numPF(N) << " prime factors\n";
+
+  vll factors = primeFactors(N);
+
+  if (!factors.empty()) {
+    cout << "They are: ";
+    for (auto x : factors) cout << x << " ";
+    cout << "\n";
+  }
+
+  cout << N << " has " << numDiv(N) << " divisors\n"; 
+
+  cout << "The sum of the divisors of " << N << " is " << sumDiv(N) << "\n";
+
+  cout << "eulerPhi(" << N << ") = " << eulerPhi(N) << "\n";
 
   return 0;
 }
