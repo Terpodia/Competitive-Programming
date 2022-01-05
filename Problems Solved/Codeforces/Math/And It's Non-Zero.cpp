c71d44b92ca74c6d1cd9453507e6309ff29b4900
terpodia <diegortizte@gmail.com>
2022-01-05 14:20:04 -0400

    New problems solved

---
+++ Problems Solved/Codeforces/Math/And It's Non-Zero.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int aux;
+
+const int INF = 1e9;
+
+int f(int b, int k) {
+  int d = (k + 1) / (1 << (b + 1));
+  int r = (k + 1) % (1 << (b + 1));
+
+  int ret = d * (1 << b);
+
+  ret += max(0, r - (1 << b));
+
+  return ret;
+}
+
+int main() {
+  int tc; aux=scanf("%d",&tc);
+  while (tc--) {
+    int l, r; aux=scanf("%d%d",&l,&r);
+    int ans=INF;
+    for (int i=0; i<30; i++) {
+      ans = min(ans, r-l+1 - f(i, r) + f(i, l-1));
+    } 
+    printf("%d\n", ans);
+  }
+
+  return 0;
+}
+
---
+++ Problems Solved/Codeforces/Math/Infinite Fence.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int aux;
+
+typedef long long ll;
+
+int main() {
+  int tc; aux=scanf("%d",&tc);
+  while (tc--) {
+    ll r, b, k;
+    aux=scanf("%lld%lld%lld",&r,&b,&k);
+
+    if (r > b) swap(r, b);
+
+    ll g = __gcd(r, b);
+    r /= g; b /= g;
+
+    if (b <= (k-1) * r + 1) puts("OBEY");
+    else puts("REBEL");
+  }
+
+  return 0;
+}
+
