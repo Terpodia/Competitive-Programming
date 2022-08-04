cac7d5dce179c187e702be296c7e35711b32c2a4
terpodia <diegortizte@gmail.com>
2022-08-03 22:26:05 -0400

    Convex hull trick (static and dynamic) has been added

---
+++ Notebook/DS/convex_hull_trick_dynamic.cpp
@@ -0,0 +1,72 @@
+// Minimum queries, the lines are added in descending order of slope (m)
+// The lines are of the form y = mx + b, and x must be non negative
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+const ll INF = 1e18;
+
+struct Line {
+	bool type;
+	ll m, b;
+	ld p;
+	bool operator<(const Line& l) const { 
+		if(!type || !l.type) return p < l.p;
+		return m > l.m; // m < l.p for maximum queries
+	}
+};
+
+struct LineContainer : set<Line> {
+	bool has_prev(set<Line>::iterator it){
+		return it != begin();
+	}
+	bool has_next(set<Line>::iterator it){
+		return it != end() && next(it) != end();
+	}
+	ld intersect(set<Line>::iterator i1, set<Line>::iterator i2){
+		return (ld)(i1->b - i2->b) / (ld)(i2->m - i1->m);
+	}
+	bool bad(set<Line>::iterator it){
+		if(has_next(it) && it->b >= next(it)->b) return true; //it->b <= next(it)->b for maximum queries
+		return (has_prev(it) && has_next(it) && 
+			intersect(prev(it),next(it)) <= intersect(prev(it),it));
+	}
+	void calc_x(set<Line>::iterator it){
+		if(has_prev(it)){
+			Line l = *it;
+			l.p = intersect(prev(it),it);
+			insert(erase(it),l);
+		}
+	}
+	void add_line(ll m, ll b){
+		set<Line>::iterator it;
+
+		it = lower_bound({1,m,b,0});
+		if(it != end() && it->m == m){
+			if(b >= it->b) return; //b <= next(it)->b for maximum queries
+			erase(it);
+		}
+
+		it = insert({1,m,b,0}).first;
+		if(bad(it)) erase(it);
+		else{
+			while(has_prev(it) && bad(prev(it))) erase(prev(it));
+			while(has_next(it) && bad(next(it))) erase(next(it));
+			if(has_next(it)) calc_x(next(it));
+			calc_x(it);
+		}
+	}
+	ll query(ll x){
+		assert(!empty());
+		Line l = *prev(upper_bound({0,0,0,(ld)x}));
+		return l.m * x + l.b;
+	}
+};
+
+int main(){
+	return 0;
+}
+
---
+++ Notebook/DS/convexhull_trick.cpp
@@ -0,0 +1,40 @@
+// Maximum queries, the lines are added in ascending order of slope (m)
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef __int128_t i128;
+
+const int MAXHSZ = 2e5;
+int hsz;
+ll hm[MAXHSZ], hb[MAXHSZ];
+
+void add_line(ll m, ll b){
+	while(hsz>1){
+		if((i128)(hb[hsz-1]-b) * (i128)(hm[hsz-1]-hm[hsz-2]) <= 
+		   (i128)(hb[hsz-2]-hb[hsz-1]) * (i128)(m-hm[hsz-1])){ 
+			hsz--; 
+		   }
+		else break;
+	}
+	hm[hsz]=m;
+	hb[hsz++]=b;
+}
+ll query(ll x){
+	int l=0, r=hsz-1;
+	while(r>l){
+		int mid=l+(r-l+1)/2;
+
+		ll m1=hm[mid], m2=hm[mid-1], b1=hb[mid], b2=hb[mid-1];
+		if(m2 - m1 < 0){swap(m2,m1); swap(m1,m2);}
+
+		if(b1-b2 <= x*(m2-m1)) l=mid;
+		else r=mid-1;
+	}
+	return hm[l] * x + hb[l];
+}
+
+int main(){
+	return 0;
+}
+
