49d680271b483438d460eb8ab9d707d0f77ee468
Diego <diegortizte@gmail.com>
2023-10-22 21:10:21 -0300

    New CSES problems solved

--- Problems Solved/CSES/Sorting and Searching/Collecting Numbers.cpp
+++ Problems Solved/CSES/Sorting and Searching/Collecting Numbers.cpp
@@ -4,16 +4,17 @@ using namespace std;
 #define fst first
 #define snd second
 
-int main(){
-  int n; scanf("%d",&n);
+int main() {
+  int n;
+  scanf("%d", &n);
   int a[n];
-  for(int i=0; i<n; i++) scanf("%d",a+i);
-  pair<int,int> c[n];
-  for(int i=0; i<n; i++) c[i] = {a[i],i};
-  sort(c,c+n);
+  for (int i = 0; i < n; i++) scanf("%d", a + i);
+  pair<int, int> c[n];
+  for (int i = 0; i < n; i++) c[i] = {a[i], i};
+  sort(c, c + n);
   int ans = 1;
-  for(int i=0; i<n-1; i++){
-    if(c[i].snd > c[i+1].snd) ans++;
+  for (int i = 0; i < n - 1; i++) {
+    if (c[i].snd > c[i + 1].snd) ans++;
   }
   printf("%d\n", ans);
   return 0;
