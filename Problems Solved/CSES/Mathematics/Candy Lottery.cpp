c038502b95493dc906759008107e9c8bacd2129b
Diegulon <diegortizte@gmail.com>
2025-01-09 23:50:15 -0300

    fixed candy lottery

--- Problems Solved/CSES/Mathematics/Candy Lottery.cpp
+++ Problems Solved/CSES/Mathematics/Candy Lottery.cpp
@@ -1,35 +1,59 @@
+#pragma GCC optimize("O3,unroll-loops")
+
 #include "bits/stdc++.h"
 using namespace std;
 
+#define fst first
+#define snd second
+#define pb push_back
+#define len(x) (int)x.size()
+#define all(x) begin(x), end(x)
+#define fore(i,a,b) for(auto i=a;i<b;i++)
+#define forn(i, n) fore(i, 0, n)
+#define dfor(i, n) for(auto i=n;i-->0;)
+#define dbg(x) cerr << "LINE" $ __LINE__ <<":" $ #x << "=" << x << endl;
+#define raya cerr << "\n-----------------------------------------------------------\n"
+#define $ <<" "<<
+
+template<typename T, typename U>
+ostream &operator<<(ostream &os, const pair<T,U> &p){
+	os << '(' << p.first $ p.second << ')';
+	return os;
+}
+template<typename T>
+ostream &operator<<(ostream &os, const vector<T> &v){
+	os << "[";
+	forn(i, len(v)){
+		if(i) os << ", ";
+		os << v[i];
+	}
+	os << "]";
+	return os;
+}
+
 typedef long double ld;
 
+ld dp[101][101];
 int n, k;
 
-ld dp[101][2];
+ld solve(int i, int j){
+	ld& ret = dp[i][j];
+	if(ret > -1) return ret;
+	ret=0;
+	if(i==n) return ret=j;
 
-int lo;
-
-ld solve(int i, int flag) {
-  if (i == n) return flag;
-  ld& ret = dp[i][flag];
-  if (ret >= 0) return ret;
-
-  if (!flag) {
-    ret = (ld)(lo - 1) / (ld)k * solve(i + 1, flag);
-    ret += solve(i + 1, 1) / (ld)k;
-  } else
-    ret = (ld)lo / (ld)k * solve(i + 1, flag);
-  return ret;
+	fore(z,1,k+1){
+		ret += solve(i+1, max(j,z)) / k;	
+	}
+	return ret;
 }
 
-int main() {
-  scanf("%d%d", &n, &k);
-  ld ans = 0;
-  for (int i = 1; i <= k; i++) {
-    for (int j = 0; j < 101; j++) dp[j][0] = dp[j][1] = -1;
-    lo = i;
-    ans += solve(0, 0) * (ld)i;
-  }
-  printf("%.6Lf\n", ans);
-  return 0;
+int main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);
+	cout << fixed << setprecision(6);
+	cin>>n>>k;
+	forn(i,101) forn(j,101) dp[i][j]=-10;
+	cout << solve(0,0) << "\n";
+	return 0;
 }
+
